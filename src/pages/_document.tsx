84299d77a0139e785c1c1531b69605f3a85e66a5
Oosasukel <rodrigo_gonn@hotmail.com>
2021-03-01 20:54:04 -0300

    typeorm config

---
+++ src/pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+interface MyDocumentProps {
+  styleTags: Array<React.ReactElement<{}>>;
+}
+
+export default class MyDocument extends Document<MyDocumentProps> {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>My page</title>
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
