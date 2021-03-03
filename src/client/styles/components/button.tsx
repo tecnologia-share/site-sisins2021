d2a3420d047f9fd033cb046906aad823e0a47fb0
Matheus Schiffer <43976069+Psychobellic@users.noreply.github.com>
2021-03-03 10:22:00 -0300

    primeiro slide do figma pt1

---
+++ src/client/styles/components/button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = ({ children }) => {
+  return <button>{children}</button>;
+};
+
+export default Button;
--- src/pages/fe.tsx
+++ src/pages/fe.tsx
@@ -1,14 +1,16 @@
-import { Container, List, Sidebar, Navbar } from "../client/styles/styles";
+import { Container, List, Sidebar, Navbar } from '../client/styles/styles';
+import Button from '../client/styles/components/button';
 
 const Fe = () => {
   {
-    document.title = "Front-End Playground";
+    document.title = 'Front-End Playground';
   }
   return (
     <Container>
       <Navbar />
       <Sidebar />
       <List />
+      <Button>My Button here</Button>
     </Container>
   );
 };
