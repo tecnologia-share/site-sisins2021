cd9e1a5d179cd4859f8c20270e4b52d36c8bd461
Matheus Schiffer <matheusschiffer@gmail.com>
2021-03-08 20:37:23 -0300

    Em src server models

---
+++ src/server/models/Participante.ts
@@ -0,0 +1,40 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('participantes')
+class Participante {
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    nome: string;
+
+    @Column()
+    senha: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    telefone: string;
+
+    @Column()
+    nascimento: Date;
+
+    @Column()
+    pais: string;
+
+    @Column()
+    cidade: string;
+
+    @Column()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { Participante };
---
+++ src/server/models/UsuarioShare.ts
@@ -0,0 +1,31 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('usuarios_share')
+class UsuarioShare {
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    senha: string;
+
+    @Column()
+    role: string;
+
+    @Column()
+    nome: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { UsuarioShare };
