2086064775581f14afaa6c58bd177f37a70baf58
Renan <renan04lima@outlook.com>
2021-03-09 20:01:35 -0300

    Migration Curso, Inscrição, Avaliações e Perguntas

--- src/server/database/migrations/1615329064778-CreateCurso.ts
+++ src/server/database/migrations/1615329064778-CreateCurso.ts
@@ -1,11 +1,66 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateCurso1615329064778 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'cursos',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'processo_seletivo_id',
+            type: 'varchar',
+          },
+          {
+            name: 'tipo',
+            type: 'varchar',
+          },
+          {
+            name: 'nome',
+            type: 'varchar',
+          },
+          {
+            name: 'professor',
+            type: 'varchar',
+          },
+          {
+            name: 'descricao',
+            type: 'varchar',
+          },
+          {
+            name: 'has_prova',
+            type: 'boolean',
+          },
+          {
+            name: 'horario',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKProcessosSeletivos',
+            referencedTableName: 'processos_seletivos',
+            referencedColumnNames: ['id'],
+            columnNames: ['processo_seletivo_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cursos');
+  }
 }
--- src/server/database/migrations/1615329083096-CreatePergunta.ts
+++ src/server/database/migrations/1615329083096-CreatePergunta.ts
@@ -1,11 +1,56 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreatePergunta1615329083096 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'perguntas',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'pergunta',
+            type: 'varchar',
+          },
+          {
+            name: 'tipo',
+            type: 'varchar',
+          },
+          {
+            name: 'alternativa1',
+            type: 'varchar',
+          },
+          {
+            name: 'alternativa2',
+            type: 'varchar',
+          },
+          {
+            name: 'alternativa3',
+            type: 'varchar',
+          },
+          {
+            name: 'alternativa4',
+            type: 'varchar',
+          },
+          {
+            name: 'alternativa5',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('perguntas');
+  }
 }
