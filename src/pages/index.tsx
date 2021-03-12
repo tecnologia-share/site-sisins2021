e741c757e814d809868d63aaacbe0f78e0a65d51
Matheus Schiffer <matheusschiffer@gmail.com>
2021-03-12 14:12:39 -0300

    adapting branch to origin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-import { Nav } from '../client/components/Nav/index';
+import { Nav } from '../components/Nav/index';
+import { Container, Title } from '../styles/home/styles';
 const Home = () => {
   return <Nav />;
-=======
-import { Container, Title } from '../styles/home/styles';
-
-const Home = () => {
-  return (
-    <Container>
-      <Title>Home</Title>
-    </Container>
-  );
->>>>>>> 7d8418cc7e3c3ccc8c849daa67e702442da3bcdd
 };
 
 export default Home;
