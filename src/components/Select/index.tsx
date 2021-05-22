7310fac777bfc5d778581f32a240918146bd63e0
Oosasukel <rodrigo_gonn@hotmail.com>
2021-05-22 11:51:36 -0300

    useCallback

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -4,6 +4,7 @@ import {
   useRef,
   useState,
   KeyboardEvent,
+  useCallback,
 } from 'react';
 import {
   Container,
@@ -47,54 +48,61 @@ const Select: React.FC<SelectProps> = ({ items, value, onChange }) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const selectMenuRef = useRef<HTMLDivElement>(null);
 
-  const handleSelectControlClick: MouseEventHandler<HTMLDivElement> = (
-    event
-  ) => {
-    event.preventDefault();
-    event.stopPropagation();
+  const handleSelectControlClick: MouseEventHandler<HTMLDivElement> = useCallback(
+    (event) => {
+      event.preventDefault();
+      event.stopPropagation();
 
-    if (menuOpen) return;
+      if (menuOpen) return;
 
-    setMenuOpen(true);
-    setFilteredItems(items);
-    if (items.length > 0) {
-      setFilteringCurrentValue(items[0].value);
-    }
-    inputRef.current.focus();
+      setMenuOpen(true);
+      setFilteredItems(items);
+      if (items.length > 0) {
+        setFilteringCurrentValue(items[0].value);
+      }
+      inputRef.current.focus();
 
-    const handleOutsideSelectClick = () => {
-      closeMenu();
+      const handleOutsideSelectClick = () => {
+        closeMenu();
 
-      document.body.removeEventListener('click', handleOutsideSelectClick);
-    };
+        document.body.removeEventListener('click', handleOutsideSelectClick);
+      };
 
-    document.body.addEventListener('click', handleOutsideSelectClick);
-  };
+      document.body.addEventListener('click', handleOutsideSelectClick);
+    },
+    [items, menuOpen]
+  );
 
   const closeMenu = () => {
     setMenuOpen(false);
     inputRef.current.value = '';
   };
 
-  const handleIndicatorClick: MouseEventHandler<HTMLDivElement> = (event) => {
-    event.preventDefault();
+  const handleIndicatorClick: MouseEventHandler<HTMLDivElement> = useCallback(
+    (event) => {
+      event.preventDefault();
 
-    if (menuOpen) {
-      event.stopPropagation();
-      closeMenu();
-    }
-  };
+      if (menuOpen) {
+        event.stopPropagation();
+        closeMenu();
+      }
+    },
+    [menuOpen]
+  );
 
-  const handleItemSelect = (selectedValue: string) => {
-    setCurrentValue(selectedValue);
+  const handleItemSelect = useCallback(
+    (selectedValue: string) => {
+      setCurrentValue(selectedValue);
 
-    if (onChange) {
-      const selectedItem = items.find((item) => item.value === selectedValue);
-      onChange(selectedItem);
-    }
-  };
+      if (onChange) {
+        const selectedItem = items.find((item) => item.value === selectedValue);
+        onChange(selectedItem);
+      }
+    },
+    [items, onChange]
+  );
 
-  const filterItems = () => {
+  const filterItems = useCallback(() => {
     const inputValue = inputRef.current.value.trim();
 
     if (inputValue) {
@@ -116,103 +124,107 @@ const Select: React.FC<SelectProps> = ({ items, value, onChange }) => {
         setFilteringCurrentValue(items[0].value);
       }
     }
-  };
+  }, [items]);
 
-  const scrollToValue = (selectedItemValue: string, upToDown = true) => {
-    const currentValueDiv = selectMenuRef.current.querySelector(
-      `#select-item-${selectedItemValue}`
-    ) as HTMLDivElement;
-    const itemHeight = currentValueDiv.offsetHeight;
-    const scrollHeight = currentValueDiv.parentElement.offsetHeight;
-    const totalScrollHeight = currentValueDiv.parentElement.scrollHeight;
-    const minScrollTop = currentValueDiv.parentElement.scrollTop;
-    const maxScrollTop = minScrollTop + scrollHeight;
+  const scrollToValue = useCallback(
+    (selectedItemValue: string, upToDown = true) => {
+      const currentValueDiv = selectMenuRef.current.querySelector(
+        `#select-item-${selectedItemValue}`
+      ) as HTMLDivElement;
+      const itemHeight = currentValueDiv.offsetHeight;
+      const scrollHeight = currentValueDiv.parentElement.offsetHeight;
+      const totalScrollHeight = currentValueDiv.parentElement.scrollHeight;
+      const minScrollTop = currentValueDiv.parentElement.scrollTop;
+      const maxScrollTop = minScrollTop + scrollHeight;
 
-    const itemIsVisible = () => {
-      const itemArrayPosition = filteredItems.findIndex(
-        (item) => item.value === selectedItemValue
-      );
-      const itemScrollPosition = itemArrayPosition * itemHeight;
+      const itemIsVisible = () => {
+        const itemArrayPosition = filteredItems.findIndex(
+          (item) => item.value === selectedItemValue
+        );
+        const itemScrollPosition = itemArrayPosition * itemHeight;
 
-      if (
-        itemScrollPosition < minScrollTop ||
-        itemScrollPosition + 2 * itemHeight > maxScrollTop
-      )
-        return false;
+        if (
+          itemScrollPosition < minScrollTop ||
+          itemScrollPosition + 2 * itemHeight > maxScrollTop
+        )
+          return false;
 
-      return true;
-    };
+        return true;
+      };
 
-    if (itemIsVisible()) {
-      return;
-    }
+      if (itemIsVisible()) {
+        return;
+      }
 
-    if (upToDown) {
-      const itemInversePosition =
-        filteredItems.length -
-        filteredItems.findIndex((item) => item.value === selectedItemValue) -
-        1;
+      if (upToDown) {
+        const itemInversePosition =
+          filteredItems.length -
+          filteredItems.findIndex((item) => item.value === selectedItemValue) -
+          1;
 
-      const heightToScroll =
-        totalScrollHeight - itemInversePosition * itemHeight - scrollHeight;
+        const heightToScroll =
+          totalScrollHeight - itemInversePosition * itemHeight - scrollHeight;
 
-      currentValueDiv.parentElement.scrollTo({ top: heightToScroll });
-    } else {
-      const itemPosition = filteredItems.findIndex(
-        (item) => item.value === selectedItemValue
-      );
-
-      const heightToScroll = itemPosition * itemHeight;
-
-      currentValueDiv.parentElement.scrollTo({ top: heightToScroll });
-    }
-  };
-
-  const changeFilteringCurrentValue = (
-    event: KeyboardEvent<HTMLInputElement>
-  ) => {
-    switch (event.key) {
-      case 'ArrowUp': {
-        event.preventDefault();
-
-        const currentSelectedItemIndex = filteredItems.findIndex(
-          (item) => item.value === filteringCurrentValue
+        currentValueDiv.parentElement.scrollTo({ top: heightToScroll });
+      } else {
+        const itemPosition = filteredItems.findIndex(
+          (item) => item.value === selectedItemValue
         );
 
-        if (currentSelectedItemIndex > 0) {
-          const newFilteringValue =
-            filteredItems[currentSelectedItemIndex - 1].value;
-          setFilteringCurrentValue(newFilteringValue);
-          scrollToValue(newFilteringValue, false);
-        }
+        const heightToScroll = itemPosition * itemHeight;
 
-        break;
+        currentValueDiv.parentElement.scrollTo({ top: heightToScroll });
       }
-      case 'ArrowDown': {
-        event.preventDefault();
+    },
+    [filteredItems]
+  );
 
-        const currentSelectedItemIndex = filteredItems.findIndex(
-          (item) => item.value === filteringCurrentValue
-        );
+  const changeFilteringCurrentValue = useCallback(
+    (event: KeyboardEvent<HTMLInputElement>) => {
+      switch (event.key) {
+        case 'ArrowUp': {
+          event.preventDefault();
 
-        if (filteredItems.length > currentSelectedItemIndex + 1) {
-          const newFilteringValue =
-            filteredItems[currentSelectedItemIndex + 1].value;
-          setFilteringCurrentValue(newFilteringValue);
-          scrollToValue(newFilteringValue, true);
+          const currentSelectedItemIndex = filteredItems.findIndex(
+            (item) => item.value === filteringCurrentValue
+          );
+
+          if (currentSelectedItemIndex > 0) {
+            const newFilteringValue =
+              filteredItems[currentSelectedItemIndex - 1].value;
+            setFilteringCurrentValue(newFilteringValue);
+            scrollToValue(newFilteringValue, false);
+          }
+
+          break;
         }
-        break;
-      }
-      case 'Enter': {
-        if (filteringCurrentValue) {
-          handleItemSelect(filteringCurrentValue);
+        case 'ArrowDown': {
+          event.preventDefault();
+
+          const currentSelectedItemIndex = filteredItems.findIndex(
+            (item) => item.value === filteringCurrentValue
+          );
+
+          if (filteredItems.length > currentSelectedItemIndex + 1) {
+            const newFilteringValue =
+              filteredItems[currentSelectedItemIndex + 1].value;
+            setFilteringCurrentValue(newFilteringValue);
+            scrollToValue(newFilteringValue, true);
+          }
+          break;
         }
-        closeMenu();
-        inputRef.current.blur();
-        break;
+        case 'Enter': {
+          if (filteringCurrentValue) {
+            handleItemSelect(filteringCurrentValue);
+          }
+          closeMenu();
+          inputRef.current.blur();
+          break;
+        }
       }
-    }
-  };
+    },
+    [filteredItems, filteringCurrentValue, handleItemSelect, scrollToValue]
+  );
 
   return (
     <Container>
