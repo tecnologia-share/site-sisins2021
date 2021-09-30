a750c4051d76a559bc823cdb8c8fd0456661dc2f
boolivia <jean_wylmer@hotmail.com>
2021-09-30 20:02:05 -0300

    feat: registration success page

---
+++ src/pages/inscricoes/finalizado/index.tsx
@@ -0,0 +1,7 @@
+import Finalizado from '../../../pagesComponents/inscricoes/finalizado';
+
+const InscricoesFinzalidaoPage = () => {
+  return <Finalizado />;
+};
+
+export default InscricoesFinzalidaoPage;
