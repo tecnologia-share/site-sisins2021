f32dab4698d8051974175f1e2fde4e2e744fbc34
renan04lima <renan04lima@outlook.com>
2021-11-25 16:36:39 -0300

    feat: save token on cookies

--- src/pagesComponents/login/AuthContext.tsx
+++ src/pagesComponents/login/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
+import { setCookie } from 'nookies';
 import React, { createContext, ReactNode, useState } from 'react';
 import api from 'services/api';
-
 export interface UserCredentials {
   email: string;
   password: string;
@@ -33,7 +33,13 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   async function signIn({ email, password }: UserCredentials) {
     try {
       const { data } = await api.post('/api/authenticate', { email, password });
+      setCookie(undefined, 'share.token', data.token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+      api.defaults.headers['Authorization'] = `Bearer ${data.token}`;
       setUser({ email, token: data.token });
+
       route.push('/inscricoes');
     } catch (error) {
       console.error(error);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import { parseCookies } from 'nookies';
+
+const cookies = parseCookies();
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  headers: {
+    Authorization: `Bearer ${cookies['share.token']}`,
+  },
 });
 
 export default api;
